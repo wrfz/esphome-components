6d30eb33201582165b74017a2049cfd600499415
wrfz <77174685+wrfz@users.noreply.github.com>
2025-02-03 16:47:39 +0100

    update

--- components/daikin_rotex_uart/daikin_rotex_uart.cpp
+++ components/daikin_rotex_uart/daikin_rotex_uart.cpp
@@ -4,6 +4,7 @@
 #include "esphome/components/daikin_rotex_uart/utils.h"
 #include "esphome/components/uart/uart.h"
 
+#include <iostream>
 #include <algorithm>
 #include <memory>
 
@@ -12,8 +13,18 @@ namespace daikin_rotex_uart {
 
 static const char* TAG = "daikin_rotex_uart";
 
+static int my_log_output(const char *fmt, va_list args) {
+    return vprintf(fmt, args);
+}
+
 DaikinRotexUARTComponent::DaikinRotexUARTComponent()
 {
+    std::cout << "DaikinRotexUARTComponent::DaikinRotexUARTComponent" << std::endl;
+
+    esp_log_level_set("*", ESP_LOG_INFO);
+    esp_log_set_vprintf(my_log_output);
+
+    ESP_LOGI("aaa", "DaikinRotexUARTComponent");
 }
 
 void DaikinRotexUARTComponent::add_entity(EntityBase* pEntityBase) {
